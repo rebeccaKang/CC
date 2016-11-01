f33bf7c98f03e76fa3d349c9fc413b5cb39dd3ee
康晔钰 <18966662@qq.com>
2016-11-01 12:04:30 +0800

    Initial Commit

---
+++ CartoonCollector/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CartoonCollector
+//
+//  Created by Rebecca on 16/11/1.
+//  Copyright © 2016年 Rebecca. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
